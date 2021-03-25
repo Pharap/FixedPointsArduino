4d22b40128d6d342fb02fe74420ff4fd57e33ea1
Pharap <2933055+Pharap@users.noreply.github.com>
2021-03-25 04:00:09 +0000

    Update licence year

--- src/FixedPoints/Details.h
+++ src/FixedPoints/Details.h
@@ -1,4 +1,4 @@
-// Copyright 2017-2018 Pharap
+// Copyright 2017-2021 Pharap
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
